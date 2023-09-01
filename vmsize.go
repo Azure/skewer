4e530404fa1d2278e19e77e049db8c490608c48d
Prachi Gandhi <prachigandhi@microsoft.com>
2023-09-01 12:38:41 -0700

    feat: capitalize vmsize struct fields

--- vmsize.go
+++ vmsize.go
@@ -18,30 +18,30 @@ var skuSizeScheme = regexp.MustCompile(
 // unParsableVMSizes map holds vmSize strings that cannot be easily parsed with skuSizeScheme.
 var unParsableVMSizes = map[string]VMSizeType{
 	"M416s_8_v2": {
-		family:                      "M",
-		subfamily:                   nil,
-		cpus:                        "416",
-		cpusConstrained:             nil,
-		additiveFeatures:            []rune{'s'},
-		acceleratorType:             nil,
-		confidentialChildCapability: false,
-		version:                     "v2",
-		promoVersion:                false,
-		series:                      "Ms_v2",
+		Family:                      "M",
+		Subfamily:                   nil,
+		Cpus:                        "416",
+		CpusConstrained:             nil,
+		AdditiveFeatures:            []rune{'s'},
+		AcceleratorType:             nil,
+		ConfidentialChildCapability: false,
+		Version:                     "v2",
+		PromoVersion:                false,
+		Series:                      "Ms_v2",
 	},
 }
 
 type VMSizeType struct {
-	family                      string
-	subfamily                   *string
-	cpus                        string
-	cpusConstrained             *string
-	additiveFeatures            []rune
-	acceleratorType             *string
-	confidentialChildCapability bool
-	version                     string
-	promoVersion                bool
-	series                      string
+	Family                      string
+	Subfamily                   *string
+	Cpus                        string
+	CpusConstrained             *string
+	AdditiveFeatures            []rune
+	AcceleratorType             *string
+	ConfidentialChildCapability bool
+	Version                     string
+	PromoVersion                bool
+	Series                      string
 }
 
 // parseVMSize parses the VM size and returns the parts as a map.
@@ -66,15 +66,15 @@ func getVMSize(vmSizeName string) (*VMSizeType, error) {
 	}
 
 	// [Family] - ([A-Z]): Captures a single uppercase letter.
-	vmSize.family = parts[1]
+	vmSize.Family = parts[1]
 
 	// [Sub-family]* - ([A-Z]?): Optionally captures another uppercase letter.
 	if len(parts[2]) > 0 {
-		vmSize.subfamily = &parts[2]
+		vmSize.Subfamily = &parts[2]
 	}
 
 	// [# of vCPUs] - ([0-9]+): Captures one or more digits.
-	vmSize.cpus = parts[3]
+	vmSize.Cpus = parts[3]
 
 	// [Constrained vCPUs]*
 	// -?: Optionally captures a hyphen.
@@ -84,48 +84,48 @@ func getVMSize(vmSizeName string) (*VMSizeType, error) {
 		if err != nil {
 			return nil, fmt.Errorf("converting constrained CPUs, %w", err)
 		}
-		vmSize.cpusConstrained = &parts[4]
+		vmSize.CpusConstrained = &parts[4]
 	}
 
 	// [Additive Features]
 	// ((?:[abcdilmtspPr]+|C+|NP)?): Captures a sequence of letters representing certain attributes.
 	// It can capture combinations like 'abcdilmtspPr' or 'C+' or 'NP'.
-	vmSize.additiveFeatures = []rune(parts[5])
+	vmSize.AdditiveFeatures = []rune(parts[5])
 
 	// [Accelerator Type]*
 	// _?: Optionally captures an underscore.
 	// (?:([A-Z][0-9]+)_?)?: Optionally captures a pattern that starts with an uppercase letter followed by digits,
 	// followed by an optional underscore.
 	if len(parts[6]) > 0 {
-		vmSize.acceleratorType = &parts[6]
+		vmSize.AcceleratorType = &parts[6]
 	}
 
 	// [Confidential Child Capability]* - only AKS
 	// (_cc_)?: Optionally captures the string "cc" with underscores on both sides.
 	if parts[7] == "_cc_" {
-		vmSize.confidentialChildCapability = true
+		vmSize.ConfidentialChildCapability = true
 	}
 
 	// [Version]*
 	// Optionally captures the pattern 'v' or 'V' followed by a digit from 1 to 9.
-	vmSize.version = parts[8]
+	vmSize.Version = parts[8]
 
 	// [Promo]*
 	// (_Promo)?: Optionally captures the string "_Promo".
 	if parts[9] == "_Promo" {
-		vmSize.promoVersion = true
+		vmSize.PromoVersion = true
 	}
 
 	// [Series]
 	subfamily := ""
-	if vmSize.subfamily != nil {
-		subfamily = *vmSize.subfamily
+	if vmSize.Subfamily != nil {
+		subfamily = *vmSize.Subfamily
 	}
 	version := ""
-	if len(vmSize.version) > 0 {
-		version = "_" + vmSize.version
+	if len(vmSize.Version) > 0 {
+		version = "_" + vmSize.Version
 	}
-	vmSize.series = vmSize.family + subfamily + string(vmSize.additiveFeatures) + version
+	vmSize.Series = vmSize.Family + subfamily + string(vmSize.AdditiveFeatures) + version
 
 	return &vmSize, nil
 }
